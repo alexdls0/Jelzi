325081f8a1bef3288539f637c90ab88850f05cd8
dbarranco <dbarranco@fitness-kpi.com>
2021-01-16 10:45:37 +0100

    prueba

--- Jelzi/app/src/main/java/com/example/jelzi/Login.java
+++ Jelzi/app/src/main/java/com/example/jelzi/Login.java
@@ -21,5 +21,6 @@ public class Login extends AppCompatActivity {
                 startActivity(new Intent(Login.this, Register.class));
             }
         });
+        //hola esto es prueba
     }
 }
